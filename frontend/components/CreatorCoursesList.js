372dcc62225cab15daabd9f4bcb7ac3df569447a
Rajat Saxena <rajat@raynstudios.in>
2019-10-27 00:32:21 +0530

    Pagination fixed for /create

--- frontend/components/CreatorCoursesList.js
+++ frontend/components/CreatorCoursesList.js
@@ -2,26 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import CourseItem from './CourseItem.js'
 import { creatorCourse } from '../types.js'
-import { List, ListItem } from '@material-ui/core'
+import { List, ListItem, Button } from '@material-ui/core'
+import {
+  LOAD_MORE_TEXT
+} from '../config/strings.js'
 
 const CreatorCoursesList = (props) => {
   return (
-    <List aria-label="courses">
-      {props.courses.map(
-        course => <ListItem key={course.id} onClick={e => props.onClick(course.id)} button>
-          <CourseItem
-            course={course}
-            isPublicView={false}
-            onClick={props.onClick}/>
-        </ListItem>
-      )}
-    </List>
+    <div>
+      <List aria-label="courses">
+        {props.courses.map(
+          course =>
+          <ListItem key={course.id} onClick={e => props.onClick(course.id)} button>
+            <CourseItem
+              course={course}
+              isPublicView={false}
+              onClick={props.onClick}/>
+          </ListItem>
+        )}
+      </List>
+      <Button onClick={props.onLoadMoreClick}>
+        {LOAD_MORE_TEXT}
+      </Button>
+    </div>
   )
 }
 
 CreatorCoursesList.propTypes = {
   courses: PropTypes.arrayOf(creatorCourse),
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  onLoadMoreClick: PropTypes.func
 }
 
 export default CreatorCoursesList
