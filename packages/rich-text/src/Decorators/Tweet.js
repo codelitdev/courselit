56703dcdd162ca15b17a315353d0698748b3a180
Rajat Saxena <social@rajats.xyz>
2020-09-03 03:53:05 +0000

    WIP: tweet

--- packages/rich-text/src/Decorators/Tweet.js
+++ packages/rich-text/src/Decorators/Tweet.js
@@ -3,20 +3,59 @@
  */
 import React from "react";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { useState } from "react";
+import TweetEmbed from 'react-tweet-embed';
+
+const styles = {
+  container: {
+    textAlign: "center",
+  },
+  iframeContainer: {
+    overflow: "hidden",
+  },
+  link: {
+    display: "hidden",
+    marginTop: "1em",
+    color: "#676767",
+  },
+};
 
 const Tweet = (props) => {
+  const [tweetId, setTweetId] = useState('');
+  const twttr = window['twttr'];
+  const callbacks = [];
+
+  useEffect(() => {
+    const tokens = props.decoratedText.split('\/');
+    setTweetId(tokens[tokens.length - 1]);
+
+    if (!(twttr && twttr.ready)) {
+      addScript(`https://platform.twitter.com/widgets.js`, renderTweet)
+    }
+  }, [props.decoratedText])
+
+
+  const addScript = (src, cb) => {
+    if (callbacks.length === 0) {
+      callbacks.push(cb)
+      var s = document.createElement('script')
+      s.setAttribute('src', src)
+      s.onload = () => callbacks.forEach((cb) => cb())
+      document.body.appendChild(s)
+    } else {
+      callbacks.push(cb)
+    }
+  }
+
+  const renderTweet = () => {
+    
+  }
+
   return (
     <div style={styles.container}>
       <div style={styles.iframeContainer}>
-        <iframe
-          src={`https://www.youtube.com/embed/${getVideoID(
-            props.decoratedText
-          )}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          style={styles.iframe}
-        />
+        <TweetEmbed id={tweetId} />
       </div>
       <a href={props.decoratedText} style={styles.link}>
         {props.children}
