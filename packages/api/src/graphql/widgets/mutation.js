24aa8a4fd8027a436e993eec006a173fb61a8f3e
Rajat Saxena <social@rajats.xyz>
2020-09-17 17:23:24 +0000

    Widgets: Backend code written but not tested

---
+++ packages/api/src/graphql/widgets/mutation.js
@@ -0,0 +1,26 @@
+const graphql = require("graphql");
+const types = require("./types.js");
+const logic = require("./logic.js");
+
+module.exports = {
+  saveWidgetSettings: {
+    type: types.widgetSettingsType,
+    args: {
+      widgetSettingsData: {
+        type: new graphql.GraphQLNonNull(types.widgetSettingsInputType),
+      },
+    },
+    resolve: async (root, { widgetSettingsData }, ctx) =>
+      logic.saveWidgetSettings(widgetSettingsData, ctx),
+  },
+  saveWidgetData: {
+    type: graphql.GraphQLBoolean,
+    args: {
+      widgetData: {
+        type: new graphql.GraphQLNonNull(types.widgetDataInputType),
+      },
+    },
+    resolve: async (root, { widgetData }, ctx) =>
+      logic.saveWidgetData(widgetData, ctx),
+  },
+};
