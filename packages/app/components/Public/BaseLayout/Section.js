1bb40cfcdac59faf4bcbbb1ae2c6b40942692809
Rajat Saxena <social@rajats.xyz>
2020-09-21 19:22:20 +0000

    Added excludeFromPaths to the widget spec

--- packages/app/components/Public/BaseLayout/Section.js
+++ packages/app/components/Public/BaseLayout/Section.js
@@ -3,18 +3,26 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Grid } from "@material-ui/core";
 import WidgetByName from "./WidgetByName";
+import { useRouter } from "next/router";
+import widgets from "../../../config/widgets";
 
 const Section = (props) => {
   const { name, layout } = props;
   const sectionLayout = layout[name];
+  const router = useRouter();
 
   return sectionLayout && sectionLayout.length ? (
     <Grid container item direction="column">
-      {sectionLayout.map((item, index) => (
-        <Grid item key={index}>
-          <WidgetByName name={item} />
-        </Grid>
-      ))}
+      {sectionLayout.map((item, index) =>
+        widgets[item].metadata.excludeFromPaths &&
+        widgets[item].metadata.excludeFromPaths.includes(router.pathname) ? (
+          <div key={index}></div>
+        ) : (
+          <Grid item key={index}>
+            <WidgetByName name={item} />
+          </Grid>
+        )
+      )}
     </Grid>
   ) : (
     <></>
--- packages/widget-buttondown/src/metadata.js
+++ packages/widget-buttondown/src/metadata.js
@@ -3,4 +3,5 @@ export default {
   displayName: "Buttondown 2",
   compatibleWith: ["bottom", "aside"],
   icon: "https://buttondown.email/static/images/icons/icon@72.png",
+  excludeFromPaths: ["/post/[id]/[slug]", "/login"],
 };
