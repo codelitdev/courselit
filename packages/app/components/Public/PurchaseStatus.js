e4756325745113aa8dc0c79b501bfbadd171b380
Rajat Saxena <social@rajats.xyz>
2020-10-06 00:03:12 +0530

    Payments via Stripe (#76)
    * Widgets (#74)
    
    * Text Editor: Custom Styles & Tweet Embeds (#69)
    
    * WIP: Tweets
    
    * WIP: tweet
    
    * Tweets almost work without proper styling
    
    * Tweet display with lesser bugs
    
    * Tweet Embedding
    
    * Embedding Tweets
    
    * v0.2.6-alpha.0
    
    * Font changes
    
    * TextEditor: Custom styling and Tweet embed
    
    * Reverted packages
    
    * v0.2.7-alpha.0
    
    * v0.2.6
    
    * Correct version of rich text
    
    * v0.2.7
    
    * Install only latest
    
    * v0.2.8
    
    * v0.2.9
    
    * Widgets: Backend code written but not tested
    
    * Tested backend code for widgets
    
    * Widgets appear in Layout section and public pages
    
    * Widgets: Public component working
    
    * Widget System: Button down widget ready
    
    * Removed dummy data
    
    * Added excludeFromPaths to the widget spec
    
    * Added widget readme.
    
    * Added more sections to widgets readme.
    
    * Updated readmes, re-arranged menu items
    
    * v0.2.10
    
    * v0.2.11
    
    * Widgets (#71)
    
    * Bump bl from 2.2.0 to 2.2.1 (#68)
    
    Bumps [bl](https://github.com/rvagg/bl) from 2.2.0 to 2.2.1.
    - [Release notes](https://github.com/rvagg/bl/releases)
    - [Commits](https://github.com/rvagg/bl/compare/v2.2.0...v2.2.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Text Editor: Custom Styles & Tweet Embeds (#70)
    
    * Text Editor: Custom Styles & Tweet Embeds (#69)
    
    * WIP: Tweets
    
    * WIP: tweet
    
    * Tweets almost work without proper styling
    
    * Tweet display with lesser bugs
    
    * Tweet Embedding
    
    * Embedding Tweets
    
    * v0.2.6-alpha.0
    
    * Font changes
    
    * TextEditor: Custom styling and Tweet embed
    
    * Reverted packages
    
    * v0.2.7-alpha.0
    
    * v0.2.6
    
    * Correct version of rich text
    
    * v0.2.7
    
    * Install only latest
    
    * v0.2.8
    
    * v0.2.9
    
    Co-authored-by: Rajat Saxena <rajat@pop-os.localdomain>
    
    * Widgets: Backend code written but not tested
    
    * Tested backend code for widgets
    
    * Widgets appear in Layout section and public pages
    
    * Widgets: Public component working
    
    * Widget System: Button down widget ready
    
    * Removed dummy data
    
    * Added excludeFromPaths to the widget spec
    
    * Added widget readme.
    
    * Added more sections to widgets readme.
    
    * Updated readmes, re-arranged menu items
    
    * v0.2.10
    
    * v0.2.11
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Rajat Saxena <rajat@pop-os.localdomain>
    
    * Fixed the design bug for the widget system
    
    * Prettier fixes
    
    * v0.2.12
    
    Co-authored-by: Rajat Saxena <rajat@pop-os.localdomain>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Added shipping form
    
    * WIP: Stripe checkout
    
    * Dev container: Stripe CLI & Aliases
    
    * Payments: Webhook confirmation and StripeCLI 1.5.2
    
    * Stripe listen alias and Remove unused strings
    
    * Payment screen and layout fixes
    
    * Purchase end-to-end flow tested
    
    Co-authored-by: Rajat Saxena <rajat@pop-os.localdomain>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ packages/app/components/Public/PurchaseStatus.js
@@ -0,0 +1,185 @@
+import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
+import {
+  BACKEND,
+  TRANSACTION_FAILED,
+  TRANSACTION_INITIATED,
+  TRANSACTION_SUCCESS,
+} from "../../config/constants";
+import { authProps } from "../../types";
+import { Button, Grid, Typography } from "@material-ui/core";
+import {
+  TRANSACTION_STATUS_FAILED,
+  TRANSACTION_STATUS_FAILED_DETAILS,
+  TRANSACTION_STATUS_INITIATED,
+  TRANSACTION_STATUS_SUCCESS,
+  TRANSACTION_STATUS_SUCCESS_DETAILS,
+  VERIFY_PAYMENT_BUTTON,
+  VISIT_COURSE_BUTTON,
+  PURCHASE_ID_HEADER,
+} from "../../config/strings";
+import { makeStyles } from "@material-ui/styles";
+import Link from "next/link";
+import AppLoader from "../AppLoader";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    margin: theme.spacing(2),
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const PurchaseStatus = (props) => {
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const classes = useStyles();
+  const { id, source } = router.query;
+  const courseLink = source || "";
+
+  useEffect(() => {
+    if (props.auth.checked && props.auth.guest) {
+      router.push("/");
+    }
+  }, [props.auth.checked]);
+
+  useEffect(() => {
+    if (props.auth && props.auth.token) {
+      getPaymentStatus();
+    }
+  }, [props.auth.token]);
+
+  const getPaymentStatus = async () => {
+    let paymentStatus = await makePaymentStatusRequest({
+      purchaseId: id,
+      backend: BACKEND,
+      token: props.auth.token,
+    });
+
+    if (paymentStatus.status === 401) {
+      router.push("/login");
+      return;
+    }
+
+    if (paymentStatus.status === 404) {
+      router.push("/");
+      return;
+    }
+
+    try {
+      paymentStatus = await paymentStatus.json();
+      setStatus(paymentStatus.status);
+    } catch (err) {}
+  };
+
+  const makePaymentStatusRequest = async ({ purchaseId, backend, token }) => {
+    setLoading(true);
+
+    const formData = new window.FormData();
+    formData.append("purchaseid", purchaseId);
+
+    const res = await fetch(`${backend}/payment/verify`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    setLoading(false);
+
+    return res;
+  };
+
+  return (
+    <Grid container className={classes.content}>
+      {status === TRANSACTION_SUCCESS && (
+        <Grid item container direction="column" spacing={4}>
+          <Grid item>
+            <Typography variant="h1">{TRANSACTION_STATUS_SUCCESS}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary">
+              {TRANSACTION_STATUS_SUCCESS_DETAILS}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Link href={courseLink}>
+              <Button variant="contained" color="primary">
+                {VISIT_COURSE_BUTTON}
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
+      )}
+      {status === TRANSACTION_INITIATED && (
+        <>
+          {loading ? (
+            <>
+              <AppLoader />
+            </>
+          ) : (
+            <Grid item container direction="column" spacing={4}>
+              <Grid item>
+                <Typography variant="h1">
+                  {TRANSACTION_STATUS_INITIATED}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle2">
+                  {PURCHASE_ID_HEADER}: {id}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={getPaymentStatus}
+                >
+                  {VERIFY_PAYMENT_BUTTON}
+                </Button>
+              </Grid>
+            </Grid>
+          )}
+        </>
+      )}
+      {status === TRANSACTION_FAILED && (
+        <Grid item container direction="column" spacing={4}>
+          <Grid item>
+            <Typography variant="h1">{TRANSACTION_STATUS_FAILED}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary">
+              {TRANSACTION_STATUS_FAILED_DETAILS}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle2">
+              {PURCHASE_ID_HEADER}: {id}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Link href={courseLink}>
+              <Button variant="contained" color="primary">
+                {VISIT_COURSE_BUTTON}
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
+  );
+};
+
+PurchaseStatus.propTypes = {
+  auth: authProps,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PurchaseStatus);
