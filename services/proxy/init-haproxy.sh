8852776f7a5f45d8b274ae5ce9b8b5279302af3c
Rajat Saxena <social@rajats.xyz>
2020-10-20 09:59:11 +0000

    Removed Proxy service

--- services/proxy/init-haproxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -z "${DOMAIN}" ]; then
-    API_PREFIX="${API_PREFIX:=/api}" haproxy -f /usr/local/etc/haproxy/haproxy.cfg -f /usr/local/etc/haproxy/production.cfg
-else
-    API_PREFIX="${API_PREFIX:=/api}" haproxy -f /usr/local/etc/haproxy/haproxy.cfg -f /usr/local/etc/haproxy/local.cfg
-fi
