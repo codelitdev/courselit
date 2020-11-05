3964d1c5fb4bc983a8aa8b51841bc74d642887a0
Rajat Saxena <social@rajats.xyz>
2020-11-05 06:20:51 +0000

    Common Widgets Folder Created

---
+++ packages/common-widgets/src/FeaturedContent/metadata.js
@@ -0,0 +1,7 @@
+export default {
+    name: "featured-content",
+    displayName: "Featured Content",
+    compatibleWith: ["top"],
+    icon: "",
+    excludeFromPaths: ["/post/[id]/[slug]", "/login", "/course/[id]/[slug]"],
+};  
