51ab61dc9af9825fc37d60b2b144e8abc4bab1dd
Rajat Saxena <social@rajats.xyz>
2021-03-13 20:29:22 +0000

    Multi-tenant: Appended http://

--- deployment/install.sh
+++ deployment/install.sh
@@ -106,7 +106,7 @@ cat > $CONFIGHOME/Caddyfile <<EOF
 EOF
     else
 cat > $CONFIGHOME/Caddyfile <<EOF
-${DOMAIN} {
+http://${DOMAIN} {
 	reverse_proxy {\$API_PREFIX}/* backend:8000
 	reverse_proxy frontend:3000
 
