a9265eb64eac73d37a88f1d98dbbbf66022b9fd6
Rajat Saxena <social@rajats.xyz>
2022-05-04 22:37:51 +0530

    Removed lerna Using Yarn workspaces Upgraded Yarn to version 3 NPM publish on manual tag push Moved Docker image publishing to NPM publish workflow

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,14 +2,3 @@
 
 # Start MongoDB
 service mongodb start
-
-# Lerna bootstrap
-yarn install
-echo 'y' | yarn lerna clean
-yarn lerna bootstrap
-
-# Building packages
-yarn lerna run build --scope=@courselit/rich-text
-yarn lerna run build --scope=@courselit/components-library
-yarn lerna run build --scope=@courselit/common-widgets
-yarn lerna run build --scope=@courselit/widget-buttondown
