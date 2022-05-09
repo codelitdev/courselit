41fd28a6df1ef3504f249404c2e196887250de86
Rajat Saxena <hi@sub.rajatsaxena.dev>
2022-05-09 09:44:16 +0530

    Fixed a bug (#201)
    Co-authored-by: Rajat Saxena <hi@rajatsaxena.dev>

--- packages/state-management/src/store.ts
+++ packages/state-management/src/store.ts
@@ -5,13 +5,9 @@ import thunk, { ThunkDispatch } from "redux-thunk";
 import type { State } from "@courselit/common-models";
 import reducer from "./reducer";
 
-const store = createStore(reducer, applyMiddleware(thunk));
 export type AppState = State;
 export type AppDispatch = ThunkDispatch<State, null, AnyAction>;
 
-const makeStore = () => {
-  // store.subscribe(() => console.log(store.getState()));
-  return store;
-};
+const makeStore = () => createStore(reducer, applyMiddleware(thunk));
 
 export default createWrapper<Store<State>>(makeStore, { debug: false });
