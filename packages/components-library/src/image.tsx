915530f5b4c6f6520d1b7a40291e5a6847033452
Rajat Saxena <hi@rajatsaxena.dev>
2022-06-02 18:04:55 +0530

    Using next/image component for images

--- packages/components-library/src/image.tsx
+++ packages/components-library/src/image.tsx
@@ -1,35 +1,52 @@
 import * as React from "react";
-import { styled } from "@mui/system";
-
-const StyledImg = styled("img")({});
+import NextImage from 'next/image';
 
 interface ImgProps {
-  src: string;
-  isThumbnail?: boolean;
-  classes?: string;
-  alt?: string;
-  defaultImage?: string;
-  loading?: "eager" | "lazy";
+    src: string;
+    isThumbnail?: boolean;
+    classes?: string;
+    alt?: string;
+    defaultImage?: string;
+    loading?: "eager" | "lazy";
+    width?: number;
+    height?: number;
 }
 
-const Image = (props: ImgProps) => {
-  const { src, classes, alt, defaultImage, loading = "lazy" } = props;
-  const source = src || defaultImage || "/courselit_backdrop.webp";
+// Copied from: https://github.com/vercel/next.js/blob/canary/examples/image-component/pages/shimmer.js
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
 
-  return (
-    <StyledImg
-      className={classes}
-      src={source}
-      alt={alt}
-      sx={{
-        objectFit: "cover",
-        width: "100%",
-        height: "auto",
-        borderRadius: 2,
-      }}
-      loading={loading}
-    />
-  );
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
+const Image = (props: ImgProps) => {
+    const { src, alt, defaultImage, loading = "lazy", width = 700, height = 475 } = props;
+    const source = src || defaultImage || "/courselit_backdrop.webp";
+    console.log('Image', src);
+
+    return (
+        <NextImage
+            width={700}
+            height={475}
+            src={source}
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+            alt={alt}
+            priority={ loading === "eager" ? true : false }/>
+    );
 };
 
 export default Image;
