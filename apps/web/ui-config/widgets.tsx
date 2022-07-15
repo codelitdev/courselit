6f420cf5277dce5239eff8f75745121e3c13c96b
Rajat Saxena <hi@rajatsaxena.dev>
2022-07-16 02:58:21 +0530

    PageEditor: Header with login state

--- apps/web/ui-config/widgets.tsx
+++ apps/web/ui-config/widgets.tsx
@@ -2,7 +2,6 @@
 import {
     RichText,
     TaggedContent,
-    Branding,
     Footer,
     Header,
     Featured,
--- packages/common-widgets/src/header/widget.tsx
+++ packages/common-widgets/src/header/widget.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box, Button, Grid, Link as MuiLink, Typography } from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Button,
+    Grid,
+    Link as MuiLink,
+    Typography,
+} from "@mui/material";
 import { WidgetProps } from "@courselit/common-models";
 import Settings, { Link } from "./settings";
 import { Menu } from "@courselit/components-library";
@@ -60,39 +67,131 @@ export default function Widget({ state, settings }: WidgetProps<Settings>) {
                 </Grid>
             </Grid>
             <Grid item>
-                <Button
-                    component="a"
-                    href="/login"
-                    sx={{
-                        color: settings.loginBtnColor || "white",
-                        backgroundColor: settings.loginBtnBgColor || "inherit",
-                        display: { xs: "none", lg: "flex" },
-                    }}
-                    disableElevation
-                >
-                    Login
-                </Button>
+                {state.auth.guest ? (
+                    <Box
+                        sx={{
+                            display: { xs: "none", lg: "flex" },
+                        }}
+                    >
+                        <Menu
+                            icon={
+                                <Avatar
+                                    sx={{
+                                        width: 32,
+                                        height: 32,
+                                        color:
+                                            settings.loginBtnColor || "inherit",
+                                        bgcolor:
+                                            settings.loginBtnBgColor ||
+                                            "inherit",
+                                    }}
+                                />
+                            }
+                            options={[
+                                {
+                                    label: "Login",
+                                    type: "link",
+                                    href: "/login",
+                                },
+                            ]}
+                        />
+                    </Box>
+                ) : (
+                    // <Button
+                    //     component="a"
+                    //     href="/login"
+                    //     sx={{
+                    //         color: settings.loginBtnColor || "white",
+                    //         backgroundColor:
+                    //             settings.loginBtnBgColor || "inherit",
+                    //         display: { xs: "none", lg: "flex" },
+                    //     }}
+                    //     disableElevation
+                    // >
+                    //     Login
+                    // </Button>
+                    <Box
+                        sx={{
+                            display: { xs: "none", lg: "flex" },
+                        }}
+                    >
+                        <Menu
+                            icon={
+                                <Avatar
+                                    sx={{
+                                        width: 32,
+                                        height: 32,
+                                        color:
+                                            settings.loginBtnColor || "inherit",
+                                        bgcolor:
+                                            settings.loginBtnBgColor ||
+                                            "inherit",
+                                    }}
+                                >
+                                    {state.profile.fetched
+                                        ? state.profile.email
+                                              .charAt(0)
+                                              .toUpperCase()
+                                        : ""}
+                                </Avatar>
+                            }
+                            options={[
+                                {
+                                    label: "Account",
+                                    type: "link",
+                                    href: "/account",
+                                },
+                                {
+                                    label: "Logout",
+                                    type: "link",
+                                    href: "/logout",
+                                },
+                            ]}
+                        />
+                    </Box>
+                )}
                 <Box
                     sx={{
                         display: { xs: "flex", lg: "none" },
                     }}
                 >
                     <Menu
-                        icon={<MenuIcon />}
-                        openIcon={<Close />}
+                        icon={
+                            <MenuIcon
+                                sx={{
+                                    color:
+                                        settings.loginBtnBgColor || "inherit",
+                                }}
+                            />
+                        }
+                        openIcon={
+                            <Close
+                                sx={{
+                                    color:
+                                        settings.loginBtnBgColor || "inherit",
+                                }}
+                            />
+                        }
                         options={[
                             ...settings.links.map((link) => ({
                                 label: link.label,
                                 type: "link" as "link",
                                 href: link.href,
                             })),
+                            state.auth.guest
+                                ? undefined
+                                : {
+                                      label: "Account",
+                                      type: "link",
+                                      href: "/account",
+                                  },
                             {
-                                label: "Login",
-                                type: "button",
-                                onClick: () => {},
+                                label: state.auth.guest ? "Login" : "Logout",
+                                type: "link",
+                                href: state.auth.guest ? "/login" : "/logout",
                             },
                         ]}
-                    ></Menu>
+                    />
                 </Box>
             </Grid>
         </Grid>
