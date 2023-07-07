3b967cb0b7396529e6a4d3a53d732934011588a3
Rajat Saxena <hi@rajatsaxena.dev>
2023-07-07 16:50:26 +0530

    AWS CDN

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,7 @@
 
 const cdn = process.env.MEDIALIT_SERVER
     ? process.env.MEDIALIT_CDN || process.env.MEDIALIT_SERVER
-    : "medialit.sgp1.cdn.digitaloceanspaces.com";
+    : "medialit-prod.s3.ap-southeast-1.amazonaws.com";
 
 const nextConfig = {
     reactStrictMode: true,
