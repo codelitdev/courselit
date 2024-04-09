367b37c320e3637180970591b600fc1915fb025e
Rajat Saxena <hi@rajatsaxena.dev>
2024-04-09 13:31:02 +0530

    Fixed a warning

--- packages/components-library/src/chip.tsx
+++ packages/components-library/src/chip.tsx
@@ -6,15 +6,18 @@ interface ChipProps {
     className?: string;
 }
 
-const Chip = forwardRef(({ children, style, className }: ChipProps) => {
-    return (
-        <span
-            className={`text-xs text-slate-500 border-[1px] border-slate-500 rounded px-[4px] py-[2px] ${className}`}
-            style={{ ...style }}
-        >
-            {children}
-        </span>
-    );
-});
+const Chip = forwardRef<HTMLSpanElement, ChipProps>(
+    ({ children, style, className }: ChipProps, ref) => {
+        return (
+            <span
+                ref={ref}
+                className={`text-xs text-slate-500 border-[1px] border-slate-500 rounded px-[4px] py-[2px] ${className}`}
+                style={{ ...style }}
+            >
+                {children}
+            </span>
+        );
+    },
+);
 
 export default Chip;
