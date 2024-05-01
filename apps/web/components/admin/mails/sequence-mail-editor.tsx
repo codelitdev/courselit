217b465119a854bfe6f6a4268e430bd17d1354e6
Rajat Saxena <hi@rajatsaxena.dev>
2024-05-01 13:38:31 +0530

    WIP: Mail editor screen

--- apps/web/components/admin/mails/sequence-mail-editor.tsx
+++ apps/web/components/admin/mails/sequence-mail-editor.tsx
@@ -1,13 +1,11 @@
-import {
-    Address,
-    AppMessage,
-    Sequence,
-} from "@courselit/common-models";
+import { Address, AppMessage, Sequence } from "@courselit/common-models";
 import {
     Breadcrumbs,
+    Button,
     Form,
     FormField,
     Link,
+    Select,
 } from "@courselit/components-library";
 import { AppDispatch, AppState } from "@courselit/state-management";
 import {
@@ -16,6 +14,7 @@ import {
 } from "@courselit/state-management/dist/action-creators";
 import { renderEmailContent, FetchBuilder } from "@courselit/utils";
 import {
+    BUTTON_SAVE,
     COMPOSE_SEQUENCE_EDIT_DELAY,
     MAIL_PREVIEW_TITLE,
     MAIL_SUBJECT_PLACEHOLDER,
@@ -42,11 +41,15 @@ const SequenceMailEditor = ({
     loading,
 }: SequenceMailEditorProps) => {
     const [sequence, setSequence] = useState<Sequence>(null);
-    const [delay, setDelay] = useState<number>(null);
-    const [subject, setSubject] = useState<string>(null);
-    const [preview, setPreview] = useState<string>(null);
-    const [content, setContent] = useState<string>(null);
-    const [emailRendered, setEmailRendered] = useState<string>(null);
+    const [delay, setDelay] = useState<number>(0);
+    const [subject, setSubject] = useState<string>("");
+    const [previewText, setPreviewText] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+    const [emailRendered, setEmailRendered] = useState<string>("");
+    const [email, setEmail] = useState(null);
+    const [published, setPublished] = useState<"unpublished" | "published">(
+        null,
+    );
 
     const fetch = useMemo(
         () =>
@@ -111,10 +114,12 @@ const SequenceMailEditor = ({
                 setSequence(sequence);
                 const email = sequence.emails.find((e) => e.emailId === mailId);
                 if (email) {
+                    setEmail(email);
                     setDelay(email.delayInMillis / 86400000);
                     setSubject(email.subject);
-                    setPreview(email.previewText);
+                    setPreviewText(email.previewText || "");
                     setContent(email.content);
+                    setPublished(email.published ? "published" : "unpublished");
                 }
             }
         } catch (e: any) {
@@ -127,6 +132,93 @@ const SequenceMailEditor = ({
     useEffect(() => {
         loadSequence();
     }, [loadSequence]);
+
+    const updateMail = useCallback(async () => {
+        const query = `
+            query UpdateMail(
+                $sequenceId: String!
+                $emailId: String!
+                $subject: String
+                $content: String
+                $delayInMillis: Float
+                $templateId: String
+                $actionType: SequenceEmailActionType
+                $actionData: String
+                $published: Boolean
+                $previewText: String
+            ) {
+                sequence: updateMailInSequence(
+                    sequenceId: $sequenceId,
+                    emailId: $emailId,
+                    subject: $subject,
+                    content: $content,
+                    delayInMillis: $delayInMillis,
+                    templateId: $templateId,
+                    actionType: $actionType,
+                    actionData: $actionData,
+                    published: $published,
+                    previewText: $previewText
+                ) {
+                    sequenceId,
+                    title,
+                    emails {
+                        emailId,
+                        subject,
+                        delayInMillis,
+                        previewText,
+                        published,
+                        content
+                    },
+                    trigger {
+                        type,
+                        data
+                    },
+                    from {
+                        name,
+                        email
+                    },
+                    emailsOrder,
+                    status
+                }
+            }`;
+
+        const fetcher = fetch
+            .setPayload({
+                query,
+                variables: {
+                    sequenceId,
+                    emailId: email.emailId,
+                    subject,
+                    content,
+                    delayInMillis: delay * 86400000,
+                    published: published === "published",
+                    previewText,
+                },
+            })
+            .build();
+
+        try {
+            dispatch(networkAction(true));
+            const response = await fetcher.exec();
+            if (response.sequence) {
+                const { sequence } = response;
+                setSequence(sequence);
+                const email = sequence.emails.find((e) => e.emailId === mailId);
+                if (email) {
+                    setEmail(email);
+                    setDelay(email.delayInMillis / 86400000);
+                    setSubject(email.subject);
+                    setPreviewText(email.previewText || "");
+                    setContent(email.content);
+                    setPublished(email.published ? "published" : "unpublished");
+                }
+            }
+        } catch (e: any) {
+            dispatch(setAppMessage(new AppMessage(e.message)));
+        } finally {
+            dispatch(networkAction(false));
+        }
+    }, [dispatch, fetch, sequenceId]);
 
     return (
         <div className="flex flex-col gap-4">
@@ -144,25 +236,60 @@ const SequenceMailEditor = ({
                     {PAGE_HEADER_EDIT_MAIL}
                 </h1>
                 <div className="flex gap-2">
-                    {/*
-                    <Button onClick={() => createSequence()}>
-                        {BTN_NEW_SEQUENCE}
+                    <Button
+                        onClick={() => {}}
+                        disabled={
+                            !subject ||
+                            !content ||
+                            delay < 0 ||
+                            (subject === email.subject &&
+                                content === email.content &&
+                                delay === email.delayInMillis / 86400000 &&
+                                previewText === (email.previewText || "") &&
+                                published ===
+                                    (email.published
+                                        ? "published"
+                                        : "unpublished"))
+                        }
+                    >
+                        {BUTTON_SAVE}
                     </Button>
-                    */}
                 </div>
             </div>
             <div>
                 <Form className="flex flex-col gap-4 mb-8" onSubmit={() => {}}>
-                    <FormField
-                        type="number"
-                        min={0}
-                        value={delay}
-                        label={COMPOSE_SEQUENCE_EDIT_DELAY}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                            setDelay(+e.target.value)
-                        }
-                        endIcon={<span>days</span>}
-                    />
+                    <div className="flex gap-8">
+                        <FormField
+                            type="number"
+                            min={0}
+                            value={delay}
+                            label={COMPOSE_SEQUENCE_EDIT_DELAY}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                setDelay(+e.target.value)
+                            }
+                            endIcon={<span>days</span>}
+                            className="w-1/2"
+                        />
+                        <div className="w-1/2 self-end">
+                            <Select
+                                value={published}
+                                onChange={(
+                                    value: "unpublished" | "published",
+                                ) => setPublished(value)}
+                                title="Status"
+                                options={[
+                                    {
+                                        label: "Published",
+                                        value: "published",
+                                    },
+                                    {
+                                        label: "Unpublished",
+                                        value: "unpublished",
+                                    },
+                                ]}
+                            />
+                        </div>
+                    </div>
                     <FormField
                         value={subject}
                         label={MAIL_SUBJECT_PLACEHOLDER}
@@ -171,23 +298,66 @@ const SequenceMailEditor = ({
                         }
                     />
                     <FormField
-                        value={preview}
+                        value={previewText}
                         label={MAIL_PREVIEW_TITLE}
                         onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                            setPreview(e.target.value)
+                            setPreviewText(e.target.value)
                         }
                     />
                     <div className="flex gap-2">
-                        <div className="flex flex-col w-1/4">
+                        <div className="flex flex-col w-1/5">
                             <h3 className="text-lg font-semibold">Variables</h3>
+                            <div className="flex flex-col gap-4 border border-gray-200 rounded w-full h-[360px] p-2">
+                                <p className="text-xs text-slate-500">
+                                    You can use the following variables in your
+                                    content.
+                                </p>
+                                <p className="text-xs text-slate-500">
+                                    These will be replaced with the actual data
+                                    while sending emails.
+                                </p>
+                                <div>
+                                    <h4 className="font-semibold text-sm text-slate-600">
+                                        {"{{ subscriber.email }}"}
+                                    </h4>
+                                    <p className="text-sm text-slate-500">
+                                        The email of the subscriber
+                                    </p>
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold text-sm text-slate-600">
+                                        {"{{ subscriber.name}}"}
+                                    </h4>
+                                    <p className="text-sm text-slate-500">
+                                        The name of the subscriber
+                                    </p>
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold text-sm text-slate-600">
+                                        {"{{ address }}"}
+                                    </h4>
+                                    <p className="text-sm text-slate-500">
+                                        Your mailing address
+                                    </p>
+                                </div>
+                                <div>
+                                    <h4 className="font-semibold text-sm text-slate-600">
+                                        {"{{ unsubscribe_link}}"}
+                                    </h4>
+                                    <p className="text-sm text-slate-500">
+                                        A link to unsubscribe from the marketing
+                                        emails
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="w-1/2">
+                        <div className="flex-1">
                             <FormField
                                 component="textarea"
                                 value={content}
                                 label={"Editor"}
                                 multiline="true"
-                                rows={10}
+                                rows={17}
                                 onChange={(e: ChangeEvent<HTMLInputElement>) =>
                                     setContent(e.target.value)
                                 }
@@ -198,7 +368,7 @@ const SequenceMailEditor = ({
                             <iframe
                                 srcDoc={emailRendered}
                                 title="Preview"
-                                className="border border-gray-200 rounded w-full"
+                                className="border border-gray-200 rounded w-full h-[360px]"
                             />
                         </div>
                     </div>
