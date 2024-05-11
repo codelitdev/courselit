82886104927d5f0a28cd55aefe819fcc3da73061
Rajat Saxena <hi@rajatsaxena.dev>
2024-05-11 21:35:38 +0530

    Removed commented code

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const cdn = process.env.MEDIALIT_SERVER
-//     ? process.env.MEDIALIT_CDN || process.env.MEDIALIT_SERVER
-//     : "medialit-prod.s3.ap-southeast-1.amazonaws.com";
-
 const remotePatterns = [
     {
         protocol: "https",
