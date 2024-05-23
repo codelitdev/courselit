fb81b63aabd30669a68588d0f0eadd4131eff477
Ravi Rajput <ravi81rajput@gmail.com>
2024-05-23 15:39:20 +0530

    Fixed header login button

--- packages/common-widgets/src/header/defaults.ts
+++ packages/common-widgets/src/header/defaults.ts
@@ -3,4 +3,4 @@ export const verticalPadding = 16;
 export const spacingBetweenLinks = 16;
 export const linkFontWeight = "font-normal";
 export const linkAlignment = "left";
-export const showLoginControl = true;
+export const showLoginControl = false;
