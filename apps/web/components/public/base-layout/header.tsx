df89eb1058b7dd417c3e462727c85cb706e9d626
Ravi Rajput <121918062+RaviRajput10@users.noreply.github.com>
2024-06-17 23:36:54 +0530

    Replaced Sign Out button with Exit course (#368)

--- apps/web/components/public/base-layout/header.tsx
+++ apps/web/components/public/base-layout/header.tsx
@@ -3,12 +3,22 @@ import { IconButton } from "@courselit/components-library";
 import { Menu } from "@courselit/icons";
 import SessionButton from "../session-button";
 import Branding from "./branding";
+import ExitCourseButton from "./exit-course-button";
+import { useRouter } from "next/router";
 
 interface HeaderProps {
     onMenuClick?: (...args: any[]) => void;
 }
 
 const Header = ({ onMenuClick }: HeaderProps) => {
+    const router = useRouter();
+    const currentCoursePathName = router.pathname;
+
+    const coursePathName = [
+        "/course/[slug]/[id]",
+        "/course/[slug]/[id]/[lesson]",
+    ];
+
     return (
         <header className="flex w-full z-10 justify-between">
             {onMenuClick && (
@@ -21,7 +31,11 @@ const Header = ({ onMenuClick }: HeaderProps) => {
                 </IconButton>
             )}
             <Branding />
-            <SessionButton />
+            {coursePathName.includes(currentCoursePathName) ? (
+                <ExitCourseButton />
+            ) : (
+                <SessionButton />
+            )}
         </header>
     );
 };
