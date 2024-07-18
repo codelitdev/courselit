55b0e8a683bdbe0ea900a708eddadcdf3350acc8
Rajat Saxena <hi@rajatsaxena.dev>
2024-07-18 14:24:44 +0530

    Release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide 'patch' or 'minor' as an argument."
+    exit 1
+fi
+
+# Check if the argument is valid
+if [ "$1" != "patch" ] && [ "$1" != "minor" ]; then
+    echo "Error: Invalid argument. Please use 'patch' or 'minor'."
+    exit 1
+fi
+
+# update the version
+yarn workspaces foreach version "$1"
+
+# get the new version
+VERSION=$(grep -m1 '"version":' apps/web/package.json | cut -d'"' -f4)
+
+# commit, tag and push
+git add . 
+git commit -m $VERSION
+git push
+git tag -a "v${VERSION}" -m "${VERSION}"
+git push origin --tags 
