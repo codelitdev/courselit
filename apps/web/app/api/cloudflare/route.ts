477d2894b6328763a211b40d87d82e08dd73d46c
ravirajput10 <ravi81rajput@gmail.com>
2024-10-15 10:09:44 +0000

    Moved the turnstile verification code in the /api/cloudflare

--- apps/web/app/api/cloudflare/route.ts
+++ apps/web/app/api/cloudflare/route.ts
@@ -1,5 +1,37 @@
 import { NextRequest } from "next/server";
-import { validateTurnstileToken } from "@courselit/utils";
+import { error } from "@/services/logger";
+
+const validateTurnstileToken = async (token: string) => {
+    const SECRET_KEY = process.env.TURNSTILE_SECRET_KEY;
+
+    if (!SECRET_KEY) {
+        throw new Error("Turnstile: No secret key found");
+    }
+
+    const formData = new FormData();
+    formData.append("secret", SECRET_KEY || "");
+    formData.append("response", token || "");
+
+    const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+    const result = await fetch(url, {
+        body: formData,
+        method: "POST",
+    });
+
+    const outcome = await result.json();
+    if (outcome.success) {
+        return true;
+    }
+    if (
+        outcome["error-codes"] &&
+        outcome["error-codes"][0] === "timeout-or-duplicate"
+    ) {
+        return true;
+    }
+
+    error(`Turnstile validation failed`, outcome);
+    return false;
+};
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
