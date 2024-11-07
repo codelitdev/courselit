86f66c0e7bbec363ee7bac591f6fe2010f485c41
Rajat Saxena <hi@sub.rajatsaxena.dev>
2024-11-07 23:18:25 +0530

    Issue 375 1 (#517)
    * Added cloudflare turnstile
    
    * Moved the turnstile verification code in the /api/cloudflare
    
    * Types relaxed
    
    * Deleted yarn.lock
    
    * Corrected externals
    
    * lint fixes
    
    * next config update
    
    * dynamic route
    
    ---------
    
    Co-authored-by: ravirajput10 <ravi81rajput@gmail.com>
    Co-authored-by: Ravi Rajput <121918062+ravirajput10@users.noreply.github.com>
    Co-authored-by: Rajat Saxena <hi@rajatsaxena.dev>

--- apps/web/app/api/config/route.ts
+++ apps/web/app/api/config/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
     return Response.json(
         { turnstileSiteKey: process.env.TURNSTILE_SITE_KEY },
