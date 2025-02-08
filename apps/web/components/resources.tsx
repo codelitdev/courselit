162b792419f46500ee7982fc0213b2b884f06ccd
Rajat Saxena <hi@sub.rajatsaxena.dev>
2025-02-08 11:54:56 +0530

    Added docs (#552)
    Co-authored-by: Rajat Saxena <hi@rajatsaxena.dev>

---
+++ apps/web/components/resources.tsx
@@ -0,0 +1,29 @@
+interface Link {
+    href: string;
+    text: string;
+}
+
+interface ResourcesProps {
+    links: Link[];
+}
+
+export default function Resources({ links }: ResourcesProps) {
+    return (
+        <div className="mb-8">
+            <h2 className="font-semibold text-xl mt-8 mb-2">Resources</h2>
+            {links.map((link, index) => (
+                <>
+                    -{" "}
+                    <a
+                        key={index}
+                        href={link.href}
+                        className="text-blue-600 hover:underline"
+                        target="_blank"
+                    >
+                        {link.text}
+                    </a>
+                </>
+            ))}
+        </div>
+    );
+}
