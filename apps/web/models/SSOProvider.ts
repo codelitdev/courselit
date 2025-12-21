cf72b766b51608d9f11a6317c0a5d1ea2090cacb
Rajat <hi@rajatsaxena.dev>
2025-12-21 23:31:51 +0530

    Removed id field

--- apps/web/models/SSOProvider.ts
+++ apps/web/models/SSOProvider.ts
@@ -3,19 +3,20 @@ import mongoose from "mongoose";
 const SSOProviderSchema = new mongoose.Schema(
     {
         domain: { type: mongoose.Schema.Types.ObjectId, required: true },
-        id: { type: String, unique: true, required: true },
+        providerId: { type: String, required: true },
         issuer: { type: String },
         domain_string: { type: String },
         oidcConfig: { type: String },
         samlConfig: { type: String },
         userId: { type: String },
-        providerId: { type: String, required: true, unique: true },
         organizationId: { type: String },
     },
     {
         collection: "ssoProviders",
     },
 );
+
+SSOProviderSchema.index({ domain: 1, providerId: 1 }, { unique: true });
 
 export default mongoose.models.SSOProvider ||
     mongoose.model("SSOProvider", SSOProviderSchema);
